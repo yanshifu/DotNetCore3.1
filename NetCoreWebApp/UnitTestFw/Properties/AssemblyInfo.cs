b5a150b3974c74bd7048b3227b346b4d4a0414d3
yanshifu <yanshifu2012@hotmail.com>
2020-06-29 12:16:00 +0900

    aaa

---
+++ NetCoreWebApp/UnitTestFw/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFw")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFw")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6f46a20d-9ed9-4352-9f61-52ee57d881db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NetCoreWebApp/UnitTestFw/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NetCoreWebApp.Models;
+using NUnit.Framework;
+namespace UnitTestFw
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.Sum(5, 6, 7);
+            Assert.AreEqual(18, result);
+        }
+    }
+}
